0b1c8e84a155c7d6d520869cbce34d3c5f609ae3
KimmDung <kimthithanhdung2002@gmail.com>
2023-09-18 22:46:54 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const contactRouter = require("./app/routes/contact.route");
 const cors = require("cors");
 
 const app = express();
-
+// middleware
 app.use(cors());
 app.use(express.json());
-
+// routes
+app.use("/api/contacts", contactRouter);
+// run server
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
+
+
 
 module.exports = app;
