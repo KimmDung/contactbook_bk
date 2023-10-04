290245c1a471dfd9820871bfd0f03ce4540600f5
KimmDung <kimthithanhdung2002@gmail.com>
2023-10-04 11:47:55 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,3 +1,9 @@
+ const ContactService = require("../services/contact.service");
+ const MongoDB = require("../utils/mongodb.util");
+ const ApiError = require("../api-error");
+
+
+
 exports.create = (req, res) => {
     res.send({ message: "create handler" });
 };
@@ -26,3 +32,39 @@ exports.findAllFavorite = (rep, res) => {
     res.send({ message: "findAllFavorite handler" });
 };
 
+// Create and Save a new Contact
+
+exports.create = async (rep, res, next) => {
+    if (!req.body?.name) {
+        return next(new ApiError(404, "Name can not be empty"));
+    }
+    try {
+        const ContactService = new ContactService(MongoDB.client);
+        const document = await ContactService.create(req.body);
+        return res.send(document);
+    } catch (error) {
+        return next(
+            new ApiError(500, "An error occurred while creating the contact")
+        );
+    }
+}
+
+// Retrieve all contacts of a users from the database
+exports.findAll = async (req, res, next) => {
+    let document = [];
+
+    try {
+        const ContactService = new ContactService(MongoDB.client);
+        const { name} = req.query;
+        if (name) {
+            document = await ContactService.findByName(name);
+        } else {
+            document = await ContactService.find({});
+        }
+    } catch (error) {
+        return next(
+            new ApiError(500, "An error ocurred while retrieving contacts")
+        );
+    }
+    return res.send(document);
+};
